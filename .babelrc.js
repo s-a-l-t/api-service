8ae9fdf83ec25b1403dd82f3a239365ade2db470
ptlm500 <peter_mills@hotmail.co.uk>
2020-02-22 21:53:06 +0000

    Add babel config for tests

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: ['@babel/preset-env'],
   plugins: [
-    '@babel/plugin-transform-modules-commonjs'
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-transform-runtime',
+    'babel-plugin-dynamic-import-node'
   ]
 }
